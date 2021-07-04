606131719a83dfaf25fcbab721188261b8cb20b1
wisPengrada <32670565+pengrada@users.noreply.github.com>
2021-07-04 23:07:47 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -18,7 +18,7 @@ module.exports = {
 
   // social usernames (optional)
   twitter: '',
-  github: 'transitive-bullshit',
+  github: 'pengrada',
   linkedin: '',
 
   // default notion icon and cover images for site-wide consistency (optional)
